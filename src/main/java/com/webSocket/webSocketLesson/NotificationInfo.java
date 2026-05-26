7825605540df7b969509e08d07105bafb4a97cb9
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-05-26 17:57:52 +0100

    message dto

---
+++ src/main/java/com/webSocket/webSocketLesson/NotificationInfo.java
@@ -0,0 +1,19 @@
+package com.webSocket.webSocketLesson;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationInfo {
+    // this does not have dataBase just for understanding sak
+    private  String message;
+    private double amount;
+
+
+
+}
